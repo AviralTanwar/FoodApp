40b1376eebd724ad49610c516c87d70a293459b4
AviralTanwar <aviraltanwar@gmail.com>
2023-05-03 03:00:53 +0530

    appjs

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import RootNavigation from './RootNavigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WelcomeScreen from './screens/Welcomescreen/WelcomeScreen';
+import LoginScreen from './screens/LoginSignupScreen/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import SignupScreen from './screens/LoginSignupScreen/SignupScreen';
+import AddressScreen from './screens/LoginSignupScreen/AddressScreen';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
   return (
-      <RootNavigation/>
+      <NavigationContainer>
+                <Stack.Navigator initialRouteName="welcomepage">
+        <Stack.Screen name="Welcomepage" component={WelcomeScreen} 
+            options={{headerShown: false}}
+        />
+        <Stack.Screen name="Signup" component={SignupScreen} 
+            options={{headerShown: false}}
+        />
+        <Stack.Screen name="Login" component={LoginScreen} 
+            options={{headerShown: false}}
+        />
+        <Stack.Screen name="add" component={AddressScreen} 
+            options={{headerShown: false}}
+        />
+        <Stack.Screen name="home" component={HomeScreen} 
+            options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
